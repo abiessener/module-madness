a7533383d356070a2333afff3f77072c55276336
Sam Fortin <sam.j.fortin@gmail.com>
2017-08-04 13:01:21 -0500

    Added body-parser

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var randomNumber = require('./randomNumber');
 var convertToDollars = require('./convertToDollars');
 var moduleThree = require('./moduleThree');
 
 var app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
 
 var port = 3000;
 
@@ -18,6 +21,20 @@ app.get('/moduleMadness', function(req,res){
     }
     res.send(output);
 });
+
+app.post('/random-balance', function (req, res) {
+    console.log('random-balance hit');
+
+    var min = req.body.min;
+    var max = req.body.max;
+
+    var output = {
+        string: moduleThree.accountString,
+        dollar: moduleThree.randomDollar(156301, 156301)
+    }
+    res.send(output);
+});
+
 
 app.listen(port, function(){
     console.log('Listening on port', port);    
