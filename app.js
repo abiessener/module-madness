a74fe1d93e1e1020e6cb42d5d710252f4b76b4fa
Sam Fortin <sam.j.fortin@gmail.com>
2017-08-04 11:08:13 -0500

    Adding files. Module One complete.

---
+++ app.js
@@ -0,0 +1,5 @@
+var randomNumber = require('./randomNumber');
+var convertToDollars = require('./convertToDollars');
+var moduleThree = require('./moduleThree');
+
+console.log(randomNumber(100, 1000000));
